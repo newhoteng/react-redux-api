4798f38debcf4c5011b12f80d797a14a5b08ba6e
Harriet Oteng <harriet.oteng@gmail.com>
2023-05-09 22:01:01 +0000

    Create redux store and connect to react app

---
+++ src/redux/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from '@reduxjs/toolkit';
+// import booksReducer from './books/booksSlice';
+// import categoriesReducer from './categories/categoriesSlice';
+
+export const store = configureStore({
+  reducer: {
+    // users: usersReducer,
+  },
+});
+
+export default store;
